3ae6c3c97bec870db87e8808b791d48606043069
Yuta Nakagawa <nakagawa@1-10.com>
2018-01-29 17:06:55 +0900

    add my unique assets

---
+++ Assets/MyLib/Scripts/JsonLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace MyLib
+{
+
+    public class JsonLoader : MonoBehaviour
+    {
+
+        [SerializeField, Tooltip("Relative json file path from StreamingAssets.")]
+        private string m_RelativePath = "";
+
+        private string RelativePath
+        {
+            get { return Application.streamingAssetsPath + "/" + m_RelativePath; }
+        }
+
+
+        public T Load<T> (string path = null)
+        {
+            // Arg check
+            if (string.IsNullOrEmpty(path))
+            {
+                path = RelativePath;
+            }
+
+            string jsonText = File.ReadAllText(path);
+            T t = JsonUtility.FromJson<T>(jsonText);
+
+            return t;
+        }
+
+    }
+
+}
