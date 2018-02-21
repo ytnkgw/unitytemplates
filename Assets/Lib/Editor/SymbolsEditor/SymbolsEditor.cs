bd44a7d2c9091bd3a5d3f48f6e05df4000824019
Yuta Nakagawa <nakagawa@1-10.com>
2018-02-21 23:59:39 +0900

    Start build Symbols Editor

---
+++ Assets/Lib/Editor/SymbolsEditor/SymbolsEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MyLib
+{
+	public class SymbolsEditor
+	{
+		[MenuItem("Window/Symbols Editor")]
+		public static void Open()
+		{
+			EditorWindow.GetWindow(typeof(SymbolsEditorWindow), false, "Symbols Editor");
+		}
+	}
+}
---
+++ Assets/Lib/Editor/SymbolsEditor/SymbolsEditorWindow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MyLib
+{
+	public class SymbolsEditorWindow : EditorWindow
+	{
+
+		// ---------------------------------------------
+		#region EditorWindow Behavior
+		private void OnEnable()
+		{
+			
+		}
+
+		private void OnGUI()
+		{
+
+			// Draw symbols list
+		}
+		#endregion
+
+		// ---------------------------------------------
+		#region Draw Compopnent method
+		private void DrawList()
+		{
+
+		}
+
+		private void DrawMessages()
+		{
+
+		}
+
+		private void DrawSave()
+		{
+
+		}
+		#endregion
+	}
+}
